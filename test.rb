061288172750fcb7a1ec96061bdbef0f10c52882
Schoettker <lennartschoettker@hotmail.com>
2016-05-21 23:24:50 +0200

    return encrypted string

--- test.rb
+++ test.rb
@@ -1,13 +1,23 @@
 def caesar(string, shift)
 	array = string.split("")
+	x = Array.new
+	s = Array.new
 	array.each do |letter|
-		x = Array.new
-		x << letter.ord
-=begin
-		converted_letter+=shift
-		puts converted_letter.chr
-=end
-puts x
+		if letter ==" "
+			x << letter
+		else
+			letter=letter.ord
+
+			x << letter+=shift
+		
+		end
 	end
+		x.each do |number|
+			number = number.chr
+			s << number
+		end
+
+puts s.join(" ")
 end
-caesar("abc",1)
+caesar("abc abc",1)
+caesar("What a string!", 5)

0f850d2dc4ecff3630aece858cdcaf33e74853e4
Schoettker <lennartschoettker@hotmail.com>
2016-05-22 01:57:53 +0200

    improve readability

--- test.rb
+++ test.rb
@@ -2,28 +2,28 @@ def caesar_cipher(string, shift)
 	char_array = string.split("")
 	ascii = Array.new
 	encryption = Array.new
-	char_array.each do |letter|
-		if letter.ord.between?(32,64)
-			ascii << letter
-		elsif letter.ord.between?(97,122)
-			letter=letter.ord
-			if letter+shift>122
-				a=122-letter
-				letter+=a
-				letter=96+(shift-a)
-				ascii << letter
+	char_array.each do |character|
+		if character.ord.between?(32,64)
+			ascii << character
+		elsif character.ord.between?(97,122)
+			character=character.ord
+			if character+shift>122
+				a=122-character
+				character+=a
+				character=96+(shift-a)
+				ascii << character
 			else
-			ascii << letter+=shift
+			ascii << character+=shift
 			end
-		elsif letter.ord.between?(65,90)
-			letter=letter.ord
-			if letter+shift>90
-				a=90-letter
-				letter+=a
-				letter=64+(shift-a)
-				ascii << letter
+		elsif character.ord.between?(65,90)
+			character=character.ord
+			if character+shift>90
+				a=90-character
+				character+=a
+				character=64+(shift-a)
+				ascii << character
 			else
-			ascii << letter+=shift
+			ascii << character+=shift
 			end
 		end
 	end
@@ -31,7 +31,7 @@ def caesar_cipher(string, shift)
 			encrypted_letter = number.chr
 			encryption << encrypted_letter
 		end
-
 puts encryption.join(" ")
 end
+
 caesar_cipher("What a string!", 5)

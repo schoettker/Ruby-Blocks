acca02528e13780724496eedb614bcabff6ffcb4
Odeson <odeson@hotmail.com>
2016-06-01 16:12:50 +0200

    fix missing keyword end

--- enumerables.rb
+++ enumerables.rb
@@ -20,8 +20,11 @@ module Enumerable
     self.my_each {|i| new_array << i if yield(i)}
     new_array
   end
-
-
+  def my_all?
+    true unless self.my_each {|i| yield(i) == false}
+  end
+end
+["hi","Jo"].my_all? {|k| k.size == 2 }
 #p ["hi","Jo","dddf"].my_select {|k| k.size == 2 }
 #p ["hi","Jo"].my_each {|k| p k}
 # ["hi","Jo"].each {|k| p k}
